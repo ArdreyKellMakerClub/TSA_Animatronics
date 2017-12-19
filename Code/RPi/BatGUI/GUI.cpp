f27255fb74e27c86d1cbfb1939352b3a6298e40d
daedalus_II <genericraxacoricofallapatorian@gmail.com>
2017-12-19 10:39:02 -0500

    thing

--- Code/RPi/BatGUI/GUI.cpp
+++ Code/RPi/BatGUI/GUI.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <SerialPort.h>
 #include <SerialStream.h>
+#include <SerialStreamBuf.h>>
 #define STREAM "/dev/whatever"
 #define PORT "/dev/whatever" //change in rpi
 
@@ -22,7 +23,7 @@ int main(){
 void SerialSetup(){
     rpi_stream.Open(PORT);
     rpi_stream.BaudRate( SerialStreamBuf::BAUD_9600 );    //arduino BAUD rate
-    rpi_stream.SetNumOfStopBits(1);                     //arduino stop bits
+    rpi_stream.SetNumOfStopBits(1);                       //arduino stop bits
 
 
 
