c7c712482bb55c586d6a456b30f33475f24e2bd7
daedalus_II <genericraxacoricofallapatorian@gmail.com>
2018-01-04 10:46:00 -0500

    i think things work now

--- Code/RPi/BatGUI/GUI.cpp
+++ Code/RPi/BatGUI/GUI.cpp
@@ -1,16 +1,33 @@
-#include <SerialPort.h>
+#include <SDL2/SDL.h>
+#include <stdio.h>
 #include <SerialStream.h>
 #include <SerialStreamBuf.h>
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
 
 #define STREAM "/dev/ttyS0"
 #define PORT "/dev/ttyACM0" //change in rpi
 
+using namespace std;
 using namespace LibSerial;
+
+int something = 0;
+int dist;
+
 
 
 int main(){
-    SerialPort rpi_port(PORT);
-    SerialStream rpi_stream(STREAM);
+    SerialStream rpi_stream;
+    rpi_stream.Open( STREAM );
+    rpi_stream.SetBaudRate( SerialStreamBuf::BAUD_9600 );   //arduino BAUD rate
+    rpi_stream.SetNumOfStopBits(1);                         //arduino stop bits
 
+    while(something<1000){
+        rpi_stream <<  'r';
+        rpi_stream >> dist;
+        cout << dist << endl;
+        something++;
+    }
     return 0;
 }
