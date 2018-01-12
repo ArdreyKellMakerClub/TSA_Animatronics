1059e734081df84ae9ea9257503356ac4e66721b
daedalus_II <genericraxacoricofallapatorian@gmail.com>
2018-01-12 15:14:14 -0500

    heartbeat mostly works

--- Code/RPi/BatGUI/GUI.cpp
+++ Code/RPi/BatGUI/GUI.cpp
@@ -3,28 +3,45 @@
 #include <SerialStream.h>
 #include <SerialStreamBuf.h>
 #include <iostream>
-#include <unistd.h>
-#include <cstdlib>
+#include <SDL2/SDL.h>
 
-#define STREAM "/dev/ttyS0"
-#define PORT "/dev/ttyACM0" //change in rpi
+#define PORT "/dev/ttyS0"
+#define STREAM "/dev/ttyACM0" //change in rpi
 
 using namespace std;
 using namespace LibSerial;
 
-void setup(string, string);
-void heartbeat();
+SerialStream rpi_stream;
+int setup(string, string);
+int heartbeat();
+//int display();
+
+int something = 0;
+string thing1;
+string thing2;
 
 int main(){
     setup(PORT, STREAM);
+    //display();
     heartbeat();
+    return 0;
+}
+
+int setup(string port, string stream){
+    rpi_stream.Open( stream );
+    rpi_stream.SetBaudRate( SerialStreamBuf::BAUD_9600 );   //arduino BAUD rate
+    rpi_stream.SetNumOfStopBits(1);                         //arduino stop b
 
     return 0;
 }
 
-void setup(string port, string stream){
-    SerialStream rpi_stream;
-    rpi_stream.Open( stream );
-    rpi_stream.SetBaudRate( SerialStreamBuf::BAUD_9600 );   //arduino BAUD rate
-    rpi_stream.SetNumOfStopBits(1);                         //arduino stop b
+int heartbeat(){
+    while(something<1000){
+        cin >> thing1;
+        rpi_stream <<  thing1 << endl;
+        rpi_stream >> thing2;
+        cout << thing2 << endl;
+        something++;
+    }
+    return 0;
 }
