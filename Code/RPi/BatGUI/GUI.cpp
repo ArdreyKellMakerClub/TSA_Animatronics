4c35a9b783f8ba3c0b73ff3f5832354f8caedeae
daedalus_II <genericraxacoricofallapatorian@gmail.com>
2018-02-27 14:02:20 -0500

    fixed flashing screen, added transparency FFAAFF

--- Code/RPi/BatGUI/GUI.cpp
+++ Code/RPi/BatGUI/GUI.cpp
@@ -4,6 +4,7 @@
 
 #include "arduinoserial/arduinoserial.h"
 #include "displaygui/displaygui.h"
+#include "displaygui/TexWrap.h"
 
 #define nl "\n"
 
@@ -20,39 +21,25 @@ SDL_Window* win = NULL;
 SDL_Surface* scr, *img = NULL;
 SDL_Renderer* ren = NULL;
 SDL_Texture* tex = NULL;
-SDL_Rect stretchRect;
-SDL_Rect rectymcrectface;
+
 
 int main(){
-    stretchRect.x = 0;
-    stretchRect.y = 0;
-    stretchRect.w = SCREEN_WIDTH;
-    stretchRect.h = SCREEN_HEIGHT;
-
-    rectymcrectface.x = SCREEN_WIDTH/3;
-    rectymcrectface.y = SCREEN_HEIGHT/3;
-    rectymcrectface.w = 67;
-    rectymcrectface.h = 23;
 
     StreamSetup(STREAM , rpi_stream);
     cout<<"are we connected?"<<nl;
     cout<<"connection status: "<<Heartbeat(rpi_stream)<<endl; //check connection
 
     InitDisplay(win, scr, ren);
-    SDL_Surface* background = LoadMedia(scr, "images/background_placeholder.bmp");
 
-    SDL_BlitScaled(background, NULL, scr, &stretchRect );
-    SDL_UpdateWindowSurface(win);
+    TexWrap button = TexWrap();
+    button.TexWrap::load("images/button/button_unpressed.bmp", ren);
 
-    tex = LoadTexture(ren, "images/button/button_unpressed.bmp");
-
-
+    TexWrap background = TexWrap();
+    background.TexWrap::load("images/background_placeholder.bmp", ren);
+    background.setDim(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     bool quit = false;
     SDL_Event e;
-
-    SDL_BlitSurface(img, NULL, scr, NULL );
-
 
     while(!quit){
         while(SDL_PollEvent(&e) !=0){
@@ -60,11 +47,11 @@ int main(){
                 quit = true;
         }
 
-        SDL_RenderClear(ren);
-        SDL_RenderCopy(ren, tex, nullptr, &rectymcrectface);
-
+        SDL_SetRenderDrawColor( ren, 0xFF, 0xFF, 0xFF, 0xFF );
+        SDL_RenderClear( ren );
+        background.render( 0, 0,ren );
+        button.render( 240, 190,ren);
         SDL_RenderPresent(ren);
-        SDL_UpdateWindowSurface(win);
     }
 
     CloseDisplay(scr, win);
