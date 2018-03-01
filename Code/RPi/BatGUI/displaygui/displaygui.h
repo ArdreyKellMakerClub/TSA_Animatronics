aea89eece3b95312ff954c8f15f0fd21da9824bb
daedalus_II <genericraxacoricofallapatorian@gmail.com>
2018-03-01 14:57:33 -0500

    laksjfdlakwedsjflkasdfkljasdlkfjaslkdjflk

--- Code/RPi/BatGUI/displaygui/displaygui.h
+++ Code/RPi/BatGUI/displaygui/displaygui.h
@@ -11,6 +11,8 @@ using namespace std;
 
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
+const int FRAMERATE = /*a cinematic*/ 24;
+const int SCREEN_TICKS_PER_FRAME = 1000 / FRAMERATE;
 
 int             InitDisplay(SDL_Window*&, SDL_Surface*&, SDL_Renderer*&);
 SDL_Surface*    LoadMedia(SDL_Surface*, string);
