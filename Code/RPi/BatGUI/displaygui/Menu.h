40bd47155506a242823d48fcc7bdca6ecc5a6d6f
daedalus_II <genericraxacoricofallapatorian@gmail.com>
2018-04-16 15:18:06 -0400

    added audio folder which was missing

---
+++ Code/RPi/BatGUI/displaygui/Menu.h
@@ -0,0 +1,20 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "displaygui.h"
+#include "TexWrap.h"
+#include "Button.h"
+#include "FramerateCapper.h" //for timing
+
+class Menu
+{
+    public:
+        Menu();
+        virtual ~Menu();
+    protected:
+    private:
+        //vector<Button>;
+
+};
+
+#endif // MENU_H
