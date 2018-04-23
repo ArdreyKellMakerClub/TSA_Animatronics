f0f6e6904f5aca0bb576d14620a7a082bc7cb768
daedalus_II <genericraxacoricofallapatorian@gmail.com>
2018-04-22 23:12:46 -0400

    started serial lib; only 0-9 supported

--- Code/RPi/Serial/serial/serial.cpp
+++ Code/RPi/Serial/serial/serial.cpp
@@ -1,2 +1,67 @@
 #include "serial.h"
-#include <
+
+using namespace LibSerial;
+using namespace std;
+
+
+int Heartbeat(SerialStream&);
+
+int main(){
+
+    SerialStream stream;("/dev/ttyACM1");
+    stream.Open("/dev/ttyACM1");
+    stream.SetBaudRate(SerialStreamBuf::BAUD_9600);
+    stream.SetCharSize(SerialStreamBuf::CHAR_SIZE_8 );
+    stream.SetNumOfStopBits(1);
+    stream.SetParity( SerialStreamBuf::PARITY_NONE ) ;
+
+    int ping;
+    int pong;
+
+    //Heartbeat(stream);
+
+    while (true){
+        cin>>ping;
+        stream<<ping;
+        stream>>pong;
+        cout<<pong<<nl;
+    }
+    return 0;
+}
+
+void setup(){
+
+}
+
+#include <chrono>
+#include <thread>
+
+
+using namespace std::chrono;
+using namespace std::this_thread;
+
+
+bool connected;
+char status;
+
+int Heartbeat(SerialStream& stream){
+    for(int i = 0; i<5; i++){
+        cout<<"Test "<<i<<" of 5"<<nl;
+        stream<<'1';
+        cout<<"Ping!"<<nl;
+        sleep_for(milliseconds(500));
+        stream>>status;
+        cout<<status;
+        cout<<status<<nl;
+        if(status == 1){
+            cout<<"Pong!"<<endl;
+            connected = true;
+            break;
+        }
+
+    }
+
+    if(!connected)
+        return -1; //not connected
+    return 0;
+}
--- Code/RPi/Serial/serial/serial.h
+++ Code/RPi/Serial/serial/serial.h
@@ -1,6 +1,9 @@
 #ifndef SERIAL_H_INCLUDED
 #define SERIAL_H_INCLUDED
-
+#include <SerialStream.h>
+#include <stdio.h>
+#include <iostream>
+#define nl "\n"
 
 
 #endif // SERIAL_H_INCLUDED
